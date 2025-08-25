39c3142c9568135e450a2d1f9f612af0d355ce83
Yuxun Jiang <jiang@openphase-solutions.com>
2025-08-25 14:53:06 +0200

    keymaps

--- lua/config/plugins/which-key.lua
+++ lua/config/plugins/which-key.lua
@@ -1,70 +1,100 @@
 return {
-  {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    config = function()
-      local wk = require("which-key")
-      wk.add({
-        --        { '<leader>b', group = 'Buffers' },
-        --        { '<leader>bb', desc = 'New buffer' },
-        --        { '<leader>bd', desc = 'Delete buffer' },
-        --        { '<leader>bN', desc = 'Previous buffer' },
-        --        { '<leader>bn', desc = 'Next buffer' },
-        --        { '<leader>bl', desc = 'List buffers' },
-        --        { '<leader>bf', desc = 'Telescope buffers' },
-        --        { '<leader>br', desc = 'Rename buffer' },
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		config = function()
+			local wk = require("which-key")
+			local tsbuiltin = require("telescope.builtin")
+			wk.add({
+				-- mode = { "n", "v" },
+				-- noremap = true,
+				-- silent = true,
+				-- Basics
 
-        --        { '<leader>c', group = 'Code' },
-        --        { '<leader>ca', desc = '[C]ode action' },
-        --
-        --        { '<leader>w', group = 'Windows' },
-        --        { '<leader>ws', desc = 'Split horizontally' },
-        --        { '<leader>wv', desc = 'Split vertically' },
-        --        { '<leader>wc', desc = 'Close window' },
-        --        { '<leader>wo', desc = 'Only window' },
-        --        { '<leader>wh', desc = 'Jump left window' },
-        --        { '<leader>wj', desc = 'Jump down window' },
-        --        { '<leader>wk', desc = 'Jump up window' },
-        --        { '<leader>wl', desc = 'Jump right window' },
-        --        { '<leader>w=', desc = 'Equalize window sizes' },
-        --        { '<leader>w<Up>', desc = 'Resize up' },
-        --        { '<leader>w<Down>', desc = 'Resize down' },
-        --        { '<leader>w<Left>', desc = 'Resize left' },
-        --        { '<leader>w<Right>', desc = 'Resize right' },
-        --
-        --        { '<leader>l', group = 'Toggle' },
-        --        { '<leader>lw', desc = 'Toggle line wrap' },
-        --
-        --        { '<leader>d', group = 'Diagnostics' },
-        --        { '<leader>dc', desc = 'Open diagnostic float' },
-        --        { '<leader>d]', desc = 'Next diagnostic' },
-        --        { '<leader>d[', desc = 'Previous diagnostic' },
-        --        { '<leader>da', desc = 'Set location list' },
-        --
-        --        { '<leader>e', desc = 'Toggle Neotree' },
-        --
-        --        { '<leader>f', group = '[S]earch / [F]ind (Telescope)' },
-        --        { '<leader>ff', desc = 'Search Files' },
-        --        { '<leader>f.', desc = 'Search Recent Files (history)' },
-        --        { '<leader>fw', desc = 'Search current Word' },
-        --        { '<leader>fg', desc = 'Search by Grep' },
-        --        { '<leader>fd', desc = 'Search Diagnostics' },
-        --        { '<leader>fr', desc = 'Search Resume (last results)' },
-        --        { '<leader>fh', desc = 'Search Help Tags' },
-        --        { '<leader>fk', desc = 'Search Keymaps' },
-        --        { '<leader>fs', desc = 'Search Telescope builtin' },
-        --        { '<leader>f/', desc = 'Fuzzily Search in Current Buffer' },
-        --        { '<leader>f?', desc = 'Live Grep in Open Files' },
+				---- Delete without yanking
+				{ "<leader>d", '"_d', desc = "Delete without yanking" },
+				---- Move lines up/down
+				{ "<A-j>", ":m .+1<CR>==", desc = "Move line down" },
+				{ "<A-k>", ":m .-2<CR>==", desc = "Move line up" },
+				{ "<A-j>", ":m '>+1<CR>gv=gv", desc = "Move selection down" },
+				{ "<A-k>", ":m '<-2<CR>gv=gv", desc = "Move selection up" },
+				---- Better indenting in visual mode
+				{ "<", "<gv", { desc = "Indent left and reselect" } },
+				{ ">", ">gv", { desc = "Indent right and reselect" } },
 
-        { "<leader>g",  group = "[G]oto (LSP)" },
-        { "<leader>gd", desc = "Goto Definition" },
-        { "<leader>gD", desc = "Goto Declaration" },
-        { "<leader>gr", desc = "Goto References" },
-        { "<leader>gI", desc = "Goto Implementation" },
-        { "<leader>gT", desc = "Type Definition" },
-        { "<leader>gs", desc = "Document Symbols" },
-        { "<leader>gS", desc = "Workspace Symbols" },
-      })
-    end,
-  },
+				-- Buffers
+				{ "<leader>b", group = "Buffers" },
+				{ "<leader>bn", "<cmd>bnext<cr>", desc = "Next buffer" },
+				{ "<leader>bN", "<cmd>bprevious<cr>", desc = "Previous buffer" },
+				{ "<leader>bd", "<cmd>bd<cr>", desc = "Delete buffer" },
+				{ "<leader>bl", "<cmd>buffers<cr>", desc = "List buffers" },
+				-- Windows
+				---- Window navigation
+				{ "<C-h>", "<C-w>h", desc = "Move to left window" },
+				{ "<C-j>", "<C-w>j", desc = "Move to bottom window" },
+				{ "<C-k>", "<C-w>k", desc = "Move to top window" },
+				{ "<C-l>", "<C-w>l", desc = "Move to right window" },
+				{ "<C-Up>", ":resize +2<CR>", desc = "Increase window height" },
+				{ "<C-Down>", ":resize -2<CR>", desc = "Decrease window height" },
+				{ "<C-Left>", ":vertical resize -2<CR>", desc = "Decrease window width" },
+				{ "<C-Right>", ":vertical resize +2<CR>", desc = "Increase window width" },
+				{ "<leader>w", group = "Windows" },
+				{ "<leader>wv", "<cmd>vsplit<CR>", desc = "Split window vertically" },
+				{ "<leader>ws", "<cmd>split<CR>", desc = "Split window horizontally" },
+				{ "<leader>w=", "<cmd>wincmd =<CR>", desc = "Equal size windows" },
+				{ "<leader>wc", "<cmd>close<CR>", desc = "Close the current window" },
+				{ "<leader>wo", "<cmd>only<CR>", desc = "Close the other windows" },
+				{ "<leader>wh", "<C-w>h", desc = "Jump to left window" },
+				{ "<leader>wj", "<C-w>j", desc = "Jump to down window" },
+				{ "<leader>wk", "<C-w>k", desc = "Jump to up window" },
+				{ "<leader>wl", "<C-w>l", desc = "Jump to right window" },
+				-- Telescope
+				{ "<leader>f", group = "Find" },
+				{ "<leader>ff", tsbuiltin.find_files, desc = "[F]ind [F]iles" },
+				{ "<leader>fg", tsbuiltin.live_grep, desc = "[F]ind by [G]rep" },
+				{ "<leader>fw", tsbuiltin.grep_string, desc = "[F]ind current [W]ord" },
+				{ "<leader>fd", tsbuiltin.diagnostics, desc = "[F]ind [D]iagnostics" },
+				{ "<leader>fk", tsbuiltin.keymaps, desc = "[F]ind [K]eymaps" },
+				{
+					"<leader>fb",
+					function()
+						-- You can pass additional configuration to Telescope to change the theme, layout, etc.
+						tsbuiltin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+							winblend = 10,
+							previewer = false,
+						}))
+					end,
+					desc = "Fuzzily find in current buffer",
+				},
+				{
+					"<leader>f/",
+					function()
+						tsbuiltin.live_grep({
+							grep_open_files = true,
+							prompt_title = "Live Grep in Open Files",
+						})
+					end,
+					desc = "[F]ind in Open Files",
+				},
+				-- Lsp settings
+				{ "<leader>g", group = "Goto" },
+			})
+		end,
+
+		--
+		--        { '<leader>d', group = 'Diagnostics' },
+		--        { '<leader>dc', desc = 'Open diagnostic float' },
+		--        { '<leader>d]', desc = 'Next diagnostic' },
+		--        { '<leader>d[', desc = 'Previous diagnostic' },
+		--        { '<leader>da', desc = 'Set location list' },
+		--
+		--         { "<leader>g",  group = "[G]oto (LSP)" },
+		--         { "<leader>gd", desc = "Goto Definition" },
+		--         { "<leader>gD", desc = "Goto Declaration" },
+		--         { "<leader>gr", desc = "Goto References" },
+		--         { "<leader>gI", desc = "Goto Implementation" },
+		--         { "<leader>gT", desc = "Type Definition" },
+		--         { "<leader>gs", desc = "Document Symbols" },
+		--         { "<leader>gS", desc = "Workspace Symbols" },
+	},
 }
